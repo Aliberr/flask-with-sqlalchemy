9b5db44861a1f8ab84f207f52384e176cda6f869
Ali BERROUANE <mr.berrouane@gmail.com>
2019-04-05 08:59:51 +0200

    CRUD to Heroku

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # wsgi.py
 import os
 import logging
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -12,14 +12,48 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import product_schema, products_schema
 
 
 @app.route('/hello')
 def hello():
     return "Hello World!"
 
-@app.route('/products')
+@app.route('/products', methods=['GET', 'POST'])
 def products():
-    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
-    return products_schema.jsonify(products)
+    if request.method == 'GET':
+        products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+        return products_schema.jsonify(products)
+    elif request.method == 'POST':
+        product = Product()
+        product.name = request.get_json()['name']
+        db.session.add(product)
+        db.session.commit()
+        return f'{product.name} successfully added to database', 201
+
+
+
+@app.route('/products/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def products_id(id):
+    if request.method == 'GET':
+        products_id = db.session.query(Product).get(id)
+        if products_id:
+            return product_schema.jsonify(products_id)
+        else:
+            return "Id not found", 404
+    elif request.method == 'DELETE':
+        products_id = db.session.query(Product).get(id)
+        db.session.delete(products_id)
+        db.session.commit()
+        return f'product with id: {id} successflully deleted', 200
+    elif request.method == 'PATCH':
+        products_id = db.session.query(Product).get(id)
+        products_id.name = request.get_json()['name']
+        db.session.commit()
+        return f'product with id: {id} successflully updated', 200
+    else:
+        return "Read the API documentation", 403
+
+
+
+
